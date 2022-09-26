ee0d338019f7203b8af2066df22528dc3c9ef6fd
Hugo Demaret <hugovladimir2000@gmail.com>
2022-09-26 21:05:23 +0200

    dealloc_page and alloc_page implemented

--- CODE/disk_manager/disk_manager.h
+++ CODE/disk_manager/disk_manager.h
@@ -11,16 +11,22 @@
 #include "page_id.h"
 #include "file.h"
 
+
+//file_manager.cpp
 bool exists_file(const std::string& name);
-void read_page(page_id page, std::vector<char>& buffer);
 std::ifstream get_file(std::string str);
 u_int8_t create_file(std::string str);
 std::vector<file> get_file_list();
+void save_file_list(std::vector<file> file_list);
+file init_file(uint32_t id);
+
+//disk_manager.cpp
+std::vector<page_id> get_available_page_list(std::vector<file> file_list);
+void read_page(page_id page, std::vector<char>& buffer);
 page_id alloc_page(std::vector<file> page_list);
 std::vector<page_id> get_page_list(std::vector<file> file_list);
-void save_file_list(std::vector<file> file_list);
-std::vector<page_id> get_available_page_list(std::vector<file> file_list);
 void init_pages(std::vector<file> file_list);
-file init_file(uint32_t id);
+//
+
 
 #endif //PROJET_BDDA_DEMARET_DISK_MANAGER_H
