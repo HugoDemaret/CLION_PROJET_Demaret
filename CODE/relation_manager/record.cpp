7dc8c9614764bb8291e808b1442d74c34c0e5f5a
Hugo Demaret <hugovladimir2000@gmail.com>
2022-11-27 20:21:02 +0100

    working on tp04

--- CODE/relation_manager/record.cpp
+++ CODE/relation_manager/record.cpp
@@ -18,6 +18,27 @@ void write_to_buffer(char* buffer, u_int32_t pos, record* rec){
     buffer+= pos;
     char **ptr = rec->values;
     column *col = rec->rel_info.columns;
+    //offset directory :
+    int relpos = (int)(rec->rel_info.nb_column+1)*4;
+    for (int i =0; i<rec->rel_info.nb_column; ++i){
+        if (col[i].type == INTEGER || col[i].type == REAL) {
+            write_int_to_buffer((int) relpos, buffer);
+            buffer+=4;
+            relpos+=4;
+        } else if (col[i].type == VARCHAR){
+            write_int_to_buffer((int) relpos, buffer);
+            buffer+=4;
+            relpos+=col[i].size;
+        }
+    }
+    if (col[rec->rel_info.nb_column-1].type == INTEGER || col[rec->rel_info.nb_column-1].type == REAL){
+        write_int_to_buffer((int)relpos+4,buffer);
+        buffer+=4;
+    } else if (col[rec->rel_info.nb_column-1].type == VARCHAR){
+        write_int_to_buffer((int) relpos + col[rec->rel_info.nb_column-1].size, buffer);
+        buffer+=4;
+    }
+    //values :
     for (int i=0; i<rec->rel_info.nb_column; ++i){
         if (col[i].type == INTEGER) {
             write_int_to_buffer(*(int*) ptr, buffer);
@@ -61,6 +82,17 @@ void read_from_buffer(char* buffer, u_int32_t pos, record* rec){
     }
 }
 
-int get_written_size(){
-
+int get_written_size(record* rec){
+    int size = 0;
+    size += (rec->rel_info.nb_column+1)*4;
+    for (int i = 0; i<rec->rel_info.nb_column;++i){
+        if (rec->rel_info.columns->type == INTEGER){
+            size += 4;
+        } else if (rec->rel_info.columns->type == REAL){
+            size += 4;
+        } else if (rec->rel_info.columns->type == VARCHAR){
+            size += rec->rel_info.columns->size;
+        }
+    }
+    return size;
 }
